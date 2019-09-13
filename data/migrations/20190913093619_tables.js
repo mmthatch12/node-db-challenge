3789f23ef8ee16e493b3e033af7bc6e6b3759768
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-09-13 09:45:49 -0600

    finished tasks table

--- data/migrations/20190913093619_tables.js
+++ data/migrations/20190913093619_tables.js
@@ -6,6 +6,18 @@ exports.up = function(knex) {
         tbl.string('project_name', 255).unique().notNullable()
         tbl.boolean('completed').defaultTo(false).notNullable()
     })
+    .createTable('tasks', tbl => {
+        tbl.increments();
+        tbl.text('description').notNullable()
+        tbl.boolean('task_completed').defaultTo(false).notNullable()
+        tbl.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('projects')
+            .onUpdate('CASCADE')
+            .onDelete('RESTRICT')
+    })
 };
 
 exports.down = function(knex) {
