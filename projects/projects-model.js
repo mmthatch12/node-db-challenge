a87016069c4999a0a19b9297ca6a6fe1c46d3e2e
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-09-13 10:07:10 -0600

    added skeleton functions on projects-model.js

--- projects/projects-model.js
+++ projects/projects-model.js
@@ -1,1 +1,34 @@
 const db = require('../data/db-config')
+
+module.exports = {
+    findResources,
+    addResource,
+    addProject,
+    findProjects,
+    addTask,
+    findTask
+}
+
+function findResources() {
+
+}
+
+function addResource() {
+
+}
+
+function addProject() {
+
+}
+
+function findProjects() {
+
+}
+
+function addTask() {
+
+}
+
+function findTask() {
+    
+}
--- projects/projects-router.js
+++ projects/projects-router.js
@@ -4,6 +4,9 @@ const Projects = require('./projects-model')
 
 const router = express.Router()
 
+router.get('/resources', (req, res) => {
+    Projects.
+})
 
 module.exports = router
 
