a1c684e7b561dd71750e95b0ddfeee341a87e84c
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-09-13 10:12:39 -0600

    added skeleton get and posts to projects-router, Im only using one router for mvp

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -5,7 +5,35 @@ const Projects = require('./projects-model')
 const router = express.Router()
 
 router.get('/resources', (req, res) => {
-    Projects.
+    Projects.findResources()
+})
+
+router.post('/resources', (req, res) => {
+    const rBody = req.body
+
+    Projects.addResources(rBody)
+})
+
+router.get('/', (req, res) => {
+    Projects.findProjects()
+})
+
+router.post('/', (req, res) => {
+    const pBody = req.body
+
+    Projects.addProject()
+})
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+
+    Projects.findTasks(id)
+})
+
+router.post('/:id/tasks', (req, res) => {
+    const { id } = req.params
+
+    Projects.addTask(id)
 })
 
 module.exports = router
